ee4486c8876b114ef65fae653da0c713e1d35e06
Jack Kinsey <jack.w.kinsey@gmail.com>
2022-04-21 13:49:44 -0400

    feat: updates the link to point to my profile

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
         <p>Hello, World!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.jackwkinsey.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Profile
         </a>
       </header>
     </div>
